b554479c703ed4f51d276342844963fa33a652ce
hicham-assou <hassou@helb-prigogine.be>
2022-12-21 14:55:55 +0100

    ajouter un champ date dans estate

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,5 @@
     'category': 'Uncategorized',
     'version': '15.0.0.1',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
-    'data': [
-    ]
+    'data': ["views/res_groups.xml", "views/res_partner.xml", "views/sale_order.xml"]
 }
