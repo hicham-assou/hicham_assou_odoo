f428be4d694ccf5c3227c9709cdd9ad0211bb76d
hicham-assou <hassou@helb-prigogine.be>
2022-12-21 15:37:01 +0100

    Update __manifest__.py

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -6,7 +6,7 @@
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '16.0.0.1',
-    'license': 'LGPL-3',
+    'licence': 'LGPL-3',
     'depends': ['base', 'sale_management', 'calendar', 'hr'],
     'data': ["views/res_groups.xml", "views/res_partner.xml", "views/sale_order.xml"]
 }
