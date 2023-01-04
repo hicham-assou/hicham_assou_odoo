d4de305aee50908f08c71ab169280b1030749f4c
hicham-assou <hassou@helb-prigogine.be>
2023-01-05 00:39:32 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -56,25 +56,34 @@ class SaleOrder(models.Model):
         self.state = 'approved'
 
     def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
-            if line.employee:
+            if line.employee_id:
                 start_datetime = fields.Datetime.to_string(line.training_date)
-                end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=10)
-                try:
-                    event = self.env['calendar.event'].create({
-                        'name': 'Formation - %s' % line.name,
-                        'start': start_datetime,
-                        'stop': end_datetime,
-                        'partner_ids': [(4, line.employee.id)],
-                    })
-                except Exception as e:
-                    print("Erreur lors de la création de l'événement :", e)
+                end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
+                if line.employee_id.user_id:
+                    user_id = line.employee_id.user_id.id
+                else:
+                    # utilise l'ID de l'utilisateur actuel ou définit False si tu ne veux pas d'événement dans le calendrier de quiconque
+                    user_id = self.env.user.id
+
+                vals = {
+                    'name': 'Formation - %s' % line.name,
+                    'start': start_datetime,
+                    'stop': end_datetime,
+                    'partner_ids': [(4, line.employee_id.id)],
+                    'privacy': 'confidential',
+                    'user_id': user_id,
+                }
+                event = self.env['calendar.event'].create(vals)
+                if not event:
+                    raise ValueError("L'événement n'a pas été créé correctement !")
+                if event.partner_ids != [(4, line.employee_id.id)] != [(4, line.employee_id.id)]:
+                    raise ValueError("L'événement n'a pas été attribué correctement aux participants !")
+
 
         if self.amount_total > 500:
             self.state = 'waiting_approval'
 
-
-    # L'utilisateur actuel est un gestionnaire de niveau 2 ou plus
-    # Faites quelque chose ici
 
 
