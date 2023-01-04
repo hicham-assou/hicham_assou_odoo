5b13a069bc35a960630d1abd0b51dfc8fe9772f8
hicham-assou <hassou@helb-prigogine.be>
2023-01-04 19:47:44 +0100

    add employee

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,4 +2,6 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+    employee = fields.Many2one('hr.employee', string="Employee")
     training_date = fields.Date(string="training date")
+
