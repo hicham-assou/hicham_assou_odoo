3fecd85bc0cce70ad356568e4d0cc89230354c86
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2023-01-04 21:26:28 +0100

    v&

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -26,6 +26,24 @@ from datetime import timedelta
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    state = fields.Selection([
+        ('draft', 'Brouillon'),
+        ('waiting_approval', 'En attente d\'approbation'),
+        ('approved', 'Approuvé'),
+        ('sent', 'Devis envoyé'),
+        ('sale', 'Vente'),
+        ('done', 'Terminé'),
+        ('cancel', 'Annulé'),
+    ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='draft')
+
+    def action_waiting_approval(self):
+        # Mettez la commande de vente en attente d'approbation
+        self.state = 'waiting_approval'
+
+    def action_approve(self):
+        # Approuvez la commande de vente
+        self.state = 'approved'
+
     def action_confirm(self):
         for line in self.order_line:
             if line.employee:
@@ -37,3 +55,4 @@ class SaleOrder(models.Model):
                     'stop': end_datetime,
                     'partner_ids': [(4, line.employee.id)],
                 })
+
