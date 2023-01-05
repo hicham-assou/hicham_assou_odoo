1e00219bc8dac05e5f886526d980b53f1f0fec6d
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2023-01-05 21:25:34 +0100

    Change

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -3,4 +3,7 @@ from odoo import fields, models
 
 class ResGroups(models.Model):
     _inherit = 'res.groups'
+    name = fields.Char(string='Group Name')
+    category_id = fields.Many2one('ir.module.category', string='Application')
+    manager_level = fields.Integer(string='Manager Level')
 
--- estate/models/res_users.py
+++
@@ -1,5 +0,0 @@
-from odoo import fields, models
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-    manager_level = fields.Integer(string='Manager Level', related='partner_id.manager_level')
