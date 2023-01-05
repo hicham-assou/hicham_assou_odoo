0eb10572b2dfd95058525364d32d3332de27bcd8
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2023-01-05 23:07:08 +0100

    :-1:

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -3,9 +3,6 @@ from odoo import fields, models
 
 class ResGroups(models.Model):
     _inherit = 'res.groups'
-    """
     name = fields.Char(string='Group Name')
     category_id = fields.Many2one('ir.module.category', string='Application')
     manager_level = fields.Integer(string='Manager Level')
-"""
-    users = fields.Many2many('res.users', string='Users')
