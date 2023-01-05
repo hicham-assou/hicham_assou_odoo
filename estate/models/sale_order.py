8eb26a89e3b16e95190ecd95f397177cf8e7adfe
hicham-assou <hassou@helb-prigogine.be>
2023-01-05 19:57:15 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -1,27 +1,6 @@
 from odoo import models, fields, api, exceptions
 from datetime import timedelta
 
-"""class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    state = fields.Selection([
-        ('draft', 'Brouillon'),
-        ('waiting_approval', 'En attente d\'approbation'),
-        ('approved', 'Approuvé'),
-        ('sent', 'Devis envoyé'),
-        ('sale', 'Vente'),
-        ('done', 'Terminé'),
-        ('cancel', 'Annulé'),
-    ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='draft')
-
-    def action_waiting_approval(self):
-        # Mettez la commande de vente en attente d'approbation
-        self.state = 'waiting_approval'
-
-    def action_approve(self):
-        # Approuvez la commande de vente
-        self.state = 'approved'"""
-
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -35,7 +14,6 @@ class SaleOrder(models.Model):
         ('done', 'Terminé'),
         ('cancel', 'Annulé'),
     ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='draft')
-
 
 
     def _check_required_manager_level(self):
