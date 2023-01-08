abf628866a786679eed3123dc0840a67ab2e5b76
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2023-01-08 22:45:35 +0100

    Res Groups V4

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -30,9 +30,9 @@ class SaleOrder(models.Model):
 
     def action_approve(self):
 
-        management_level_1_group = self.env['res.groups'].sudo().search([('name', '=', 'Management Level 1')])
-        management_level_2_group = self.env['res.groups'].sudo().search([('name', '=', 'Management Level 2')])
-        management_level_3_group = self.env['res.groups'].sudo().search([('name', '=', 'Management Level 3')])
+        management_level_1_group = self.env['res.groups'].sudo().search([('name', '=', 'Manager Level 1')])
+        management_level_2_group = self.env['res.groups'].sudo().search([('name', '=', 'Manager Level 2')])
+        management_level_3_group = self.env['res.groups'].sudo().search([('name', '=', 'Manager Level 3')])
 
 
         # Vérification que le niveau de gestionnaire de l'utilisateur est suffisant pour approuver la commande de vente
@@ -40,10 +40,12 @@ class SaleOrder(models.Model):
             pass
         elif self.amount_total >= 500 and self.amount_total < 2000:
             if self.env.user in management_level_1_group.users:
+                self.state = 'waiting_approval'
                 raise exceptions.ValidationError(
                     "Vous n'avez pas le niveau de gestionnaire requis pour approuver cette commande de vente")
         elif self.amount_total >= 2000:
             if self.env.user in management_level_2_group.users:
+                self.state = 'waiting_approval'
                 raise exceptions.ValidationError(
                     "Vous n'avez pas le niveau de gestionnaire requis pour approuver cette commande de vente")
         # Approuvez la commande de vente
