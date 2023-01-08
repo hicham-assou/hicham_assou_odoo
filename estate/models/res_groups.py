37bb9155eb5ae2f8d83896dfdd6a61dbde6f5d46
IsmaelLB22 <ilaarbibarghou@helb-prigogine.be>
2023-01-08 21:45:44 +0100

    Test

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -5,5 +5,3 @@ class ResGroups(models.Model):
     _inherit = 'res.groups'
     name = fields.Char(string='Name', required=True)
     category_id = fields.Many2one('ir.module.category', string='Module Category')
-    implied_ids = fields.Many2many('res.groups', 'res_groups_implied_rel', 'grp_id', 'implied_grp_id', string='Implied Groups')
-    comment = fields.Text(string='Comment')
